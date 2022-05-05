d81be5071791786d4d410b51e40fb5b7fc12cbfc
Anton Nagornyi <nagorny.anton@gmail.com>
2022-05-05 20:12:51 +0300

    Initial Commit

---
+++ Recipe App 2.0/Recipe_App_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_App_2_0App.swift
+//  Recipe App 2.0
+//
+//  Created by Anton Nagornyi on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_App_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
